645d17c2c528376bc2e46045577b13a7648565be
Samson Wu <78169553+samsonahh@users.noreply.github.com>
2024-07-24 15:23:18 -0400

    cleaned up code and prefabbed blockinteractmanager

--- Assets/Scripts/BlockInteractManager.cs
+++ Assets/Scripts/BlockInteractManager.cs
@@ -2,17 +2,19 @@ using UnityEngine;
 
 public class BlockInteractManager : MonoBehaviour
 {
+    private LineRenderer lineRenderer;
+    
+    [Header("Pointers")]
+    [SerializeField] private Transform mousePointerTransform;
+    [SerializeField] private Transform blockPointerTransform;
+
+    [Header("Block")]
     [SerializeField] private LayerMask blockLayer;
     private Block hoveredBlock;
     private Block draggedBlock;
 
     private Vector3 mouseWorldPos;
     private bool isDragging;
-
-    private LineRenderer lineRenderer;
-
-    [SerializeField] private Transform mousePointerTransform;
-    [SerializeField] private Transform blockPointerTransform;
 
     private void Awake()
     {
@@ -25,30 +27,59 @@ public class BlockInteractManager : MonoBehaviour
 
         HandleInput();
         AssignHoveredBlock();
-
-        if (Input.GetKey(KeyCode.Mouse0) && hoveredBlock != null && !isDragging)
-        {
-            draggedBlock = hoveredBlock;
-            draggedBlock.Rigidbody.gravityScale = 0;
-            draggedBlock.DragPoint.position = mouseWorldPos;
-            blockPointerTransform.position = draggedBlock.DragPoint.position;
-            blockPointerTransform.gameObject.SetActive(true);
-            isDragging = true;
-        }
-
-        if(Input.GetKeyUp(KeyCode.Mouse0) && isDragging)
-        {
-            draggedBlock.Rigidbody.gravityScale = 1;
-            draggedBlock = null;
-            blockPointerTransform.gameObject.SetActive(false);
-            isDragging = false;
-            lineRenderer.enabled = false;
-        }
-
-
     }
 
     private void FixedUpdate()
+    {
+        HandleDrag();
+    }
+
+    private void HandleInput()
+    {
+        mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouseWorldPos.z = 0;
+
+        if (Input.GetKey(KeyCode.Mouse0) && hoveredBlock != null && !isDragging)
+        {
+            StartDrag();
+            isDragging = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Mouse0) && isDragging)
+        {
+            StopDrag();
+            isDragging = false;
+        }
+    }
+
+    private void AssignHoveredBlock()
+    {
+        Collider2D hit = Physics2D.OverlapPoint(mouseWorldPos, blockLayer);
+
+        hoveredBlock = hit == null ? null : hit.gameObject.GetComponent<Block>();
+    }
+
+    private void StartDrag()
+    {
+        draggedBlock = hoveredBlock;
+        draggedBlock.Rigidbody.gravityScale = 0;
+        draggedBlock.DragPoint.position = mouseWorldPos;
+
+        blockPointerTransform.position = draggedBlock.DragPoint.position;
+        blockPointerTransform.gameObject.SetActive(true);
+    }
+
+    private void StopDrag()
+    {
+        draggedBlock.Rigidbody.gravityScale = 1;
+        draggedBlock = null;
+
+        blockPointerTransform.gameObject.SetActive(false);
+
+        lineRenderer.enabled = false;
+    }
+
+    private void HandleDrag()
     {
         if (isDragging)
         {
@@ -58,19 +89,6 @@ public class BlockInteractManager : MonoBehaviour
             Vector3 dir = mouseWorldPos - draggedBlock.DragPoint.position;
             draggedBlock.Rigidbody.AddForceAtPosition(dir, draggedBlock.DragPoint.position);
         }
-    }
-
-    private void HandleInput()
-    {
-        mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        mouseWorldPos.z = 0;
-    }
-
-    private void AssignHoveredBlock()
-    {
-        Collider2D hit = Physics2D.OverlapPoint(mouseWorldPos, blockLayer);
-
-        hoveredBlock = hit == null ? null : hit.gameObject.GetComponent<Block>();
     }
 
     private void DrawLine(Vector3 start, Vector3 end)
