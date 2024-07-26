23a61c92db44bc50f01f1c06692448fa40fc9cd5
TheStress <lieu.nelson386@gmail.com>
2024-07-26 01:50:10 -0400

    added timer to GameUI
    - added timer as win condition
    - you can turn off the win condition in game manager

--- Assets/Scripts/GameMangaer/GameManager.cs
+++ Assets/Scripts/GameMangaer/GameManager.cs
@@ -4,32 +4,42 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] bool useTimer = false;
     ResourceManager resourceManager;
     EndScreenUI endScreen;
+    WinConditionTimer winConditionTimer;
 
     private void Start() {
         // Finding objects
         resourceManager = FindObjectOfType<ResourceManager>();
         endScreen = FindObjectOfType<EndScreenUI>();
+        winConditionTimer = FindAnyObjectByType<WinConditionTimer>();
         
         resourceManager.OnReachGoal.AddListener(WinGame); // Setting listener for winning game
     }
 
     private void Update() {
+        if(useTimer) {
+            CheckTimerWin();
+        }
+
         CheckResources();
     }
 
     public void StartGame() {
         resourceManager.SetDraining(true); // Starting draining
+        winConditionTimer.SetTimerActive(true); // Starting timer
     }
 
     public void WinGame() {
         endScreen.SetEndScreen(true);
         resourceManager.SetDraining(false);
+        winConditionTimer.SetTimerActive(false);
     }
     public void LoseGame() {
         endScreen.SetEndScreen(false);
         resourceManager.SetDraining(false);
+        winConditionTimer.SetTimerActive(false);
     }
 
     private void CheckResources() {
@@ -37,6 +47,11 @@ public class GameManager : MonoBehaviour
             resourceManager.currentMentalHealth <= 0 ||
             resourceManager.currentFinancialHealth <= 0) {
             LoseGame();
+        }
+    }
+    private void CheckTimerWin() {
+        if(winConditionTimer.timer <= 0) {
+            WinGame();
         }
     }
 }
